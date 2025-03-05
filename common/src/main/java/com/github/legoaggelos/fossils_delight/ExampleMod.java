0f996c24f1644caf7aee1c56867f248e71a5f1d1
legoaggelos <christinagika@gmail.com>
2025-03-05 14:59:02 +0200

    Add files

---
+++ common/src/main/java/com/github/legoaggelos/fossils_delight/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.github.legoaggelos.fossils_delight;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "fossils_delight";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric/src/main/java/com/github/legoaggelos/fossils_delight/fabric/client/ExampleModFabricClient.java
@@ -0,0 +1,10 @@
+package com.github.legoaggelos.fossils_delight.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class ExampleModFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
